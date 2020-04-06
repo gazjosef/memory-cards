80c1759263bd689365c6240044fa8ba908b9c5d6
gazjosef <39395712+gazjosef@users.noreply.github.com>
2020-04-06 18:57:15 +1000

    Create Cards & Flip Effect w/JS

--- script.js
+++ script.js
@@ -31,3 +31,36 @@ const cardsData = [
     answer: 'thisIsAVariable'
   }
 ];
+
+// Create all cards
+function createCards() {
+  cardsData.forEach((data, index) => createCard(data, index));
+}
+
+// Creat a single card in DOM
+function createCard((data, index)) {
+  const card = document.createElement('div')
+  card.classList.add('card')
+  if(index === 0) {
+    card.classList.add('active')
+  }
+
+  card.innerHTML = `
+  <div class="inner-card">
+    <div class="inner-card-front">
+      <p>
+        ${data.question}
+      </p>
+    </div>
+    <div class="inner-card-back">
+      <p>
+        ${data.answer}
+      </p>
+    </div>
+  </div>
+  `
+  // Add to DOM cards
+  cardsEl.push(card)
+
+  cardsContainer.appendChild(card)
+}
