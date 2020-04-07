fd0e79f049805236cfe846a86a65a2f31663539a
gazjosef <39395712+gazjosef@users.noreply.github.com>
2020-04-07 17:15:06 +1000

    Set & Get Card Data From Local Storage

--- script.js
+++ script.js
@@ -122,3 +122,22 @@ showBtn.addEventListener('click', () => addContainer.classList.add('show'));
 
 // Hide add container
 hideBtn.addEventListener('click', () => addContainer.classList.remove('show'));
+
+// Add new card
+
+addCardBtn.addEventListener('click', () => {
+  const question = questionEl.value;
+  const answer = answerEl.value;
+  console.log(question, answer);
+
+  if (question.trim() && answer.trim()) {
+    const newCard = { question, answer };
+
+    createCard(newCard);
+
+    questionEl.value = '';
+    answerEl.value = '';
+
+    addContainer.classList.remove('show');
+  }
+});
